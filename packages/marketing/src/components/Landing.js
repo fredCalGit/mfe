343d0ac3c429c9634c58a1fb9f6985ad264a305d
Fred Cal <fbcmuinhos@paschoalotto.com.br>
2022-08-19 10:48:20 -0400

    changed text landing page

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -87,9 +87,7 @@ export default function Album() {
               color="textSecondary"
               paragraph
             >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
+              AWS is a pain but this is working
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justifyContent="center">
